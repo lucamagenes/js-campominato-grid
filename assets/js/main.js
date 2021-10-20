fff5b6eb00269d9d4d4ae119f263780fcbc2981d
lucamagenes <lucamgns33@gmail.com>
2021-10-20 15:09:36 +0200

    selezionato elemento dalla DOM generati elementi con loop creati elemnti aggiunta classe appesi al container

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,27 @@ in cui ogni cella contiene un numero tra quelli compresi in un range:
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
 */
+
+
+//selezionare l'elemento della DOM 
+const containerEl = document.querySelector('.container');
+console.log(containerEl);
+
+//generare gli elementi con un loop
+for (let i = 0; i < 100; i++) {
+
+    //creare gli elementi da generare
+    const cellEl = document.createElement('div');
+    console.log(cellEl);
+    //attribuire una classe all'elemto creato
+    cellEl.classList.add('grid_cell')
+    //appendere gli elementi generati all'elemento selezionato dalla DOM 
+    containerEl.append(cellEl)
+
+}
+
+
+
+
+
+//aggiungere un eventListener agli elementi
